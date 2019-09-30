7678c21ae1180f0ecaf33b86d400c97d1d3e06dd
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-09-30 23:02:13 +0900

    Update 02 Write your First Fulutter App

--- src/02 Write Your First Flutter App/write_your_first_flutter_app/lib/main.dart
+++ src/02 Write Your First Flutter App/write_your_first_flutter_app/lib/main.dart
@@ -1,20 +1,71 @@
+import 'package:english_words/english_words.dart' as prefix0;
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart'; // Add this line
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: Center(
+          child: RandomWords(),
+        
         ),
       ),
     );
   }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    // final WordPair wordPair = WordPair.random();
+    // return Text(wordPair.asPascalCase);
+
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+      );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }   
+
+  Widget _buildSuggestions() {
+  return ListView.builder(
+    padding: const EdgeInsets.all(16),
+    itemBuilder: (BuildContext _context, int i) {
+      if (i.isOdd) {
+        return Divider();
+      }
+
+      final int index = i ~/ 2;
+
+      if (index >= _suggestions.length) {
+        _suggestions.addAll(prefix0.generateWordPairs().take(10));
+      }
+      return _buildRow(_suggestions[index]);
+    }
+  );
+}
 }
